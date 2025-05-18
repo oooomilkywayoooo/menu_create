bb376cb04bbd142d54497bf1b360ac9dc6cd2b0c
milkyway <oooomilkywayoooo@gmail.com>
2025-05-18 16:18:13 +0900

    ホーム画面作成-ページタイトルとサイドバー-

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: [],
   content: ["./src/**/*.{html,js}"],
   theme: {
+    fontFamily: {
+	    Kiwi: ['Kiwi Maru', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
